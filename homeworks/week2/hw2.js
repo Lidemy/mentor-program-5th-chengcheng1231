120baacf832df7598f1d0a98971481f80cb86ee9
chencheng1231 <yuchenghsu1231@gmail.com>
2021-04-26 00:10:03 +0800

    finish week2 HW

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
 function capitalize(str) {
-  
+    First_letter_code = str[0].charCodeAt() // let First_letter_code as ASCII code of first letter
+    if ("a".charCodeAt() <= First_letter_code && First_letter_code <= "z".charCodeAt()) { // if First_letter_code is greater than "a" ASCII code  nd First_letter_code is less than "z" ASCII code
+        var Capti = String.fromCharCode(First_letter_code -32) // let Capti as capitalized letter
+        result =  Capti + str.slice(1) 
+        return result
+    } else {
+        return str
+    }
+        
 }
 
 console.log(capitalize('hello'));
